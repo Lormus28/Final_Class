ef772ddfee340ee6cd5e17186d95fda142d32e7d
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-06-24 12:01:02 -0500

    JV actualizacion

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const btn = document.querySelector(".title");
-const rickAndMorty = "https://rickandmortyapi.com/api/character";
 
-btn.addEventListener('click', () => {
-    console.log("btn");
-    callApirickAndMorty();
-});
+ const btn = document.querySelector(".title");
+ const $gridContainer = document.querySelector(".gridContainer");
+let rickAndMortyAPI = "https://rickandmortyapi.com/api/character";
 
-const callApirickAndMorty= async ()=>{
-    const res = await fetch(rickAndMorty);
-    console.log(res)
-    const data = await res.json();
-    console.log (data);
-} 
+ btn.addEventListener("click", () => {
+  console.log("btn");
+   loadcard(rickAndMortyAPI);
+ });
 
+
+async function loadcard(url) {
+  console.log ("hola");
+    try {
+        console.log ("hola");
+    let res = await fetch(url),
+    json = await res.json();
+    console.log (json);
+    $template = "";
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+    for (let i = 0; i < json.results.length; i++) {
+        const rickMorty = json.results[i];
+        console.log (json.results[i]);
+        $template += `
+                <div class="card">
+    <img src="${rickMorty.image}" alt="${rickMorty.name}">
+    <h3>${rickMorty.name}</h3>
+    <p>${rickMorty.species}</p>
+</div>`;
+
+    //   } catch (error) {
+    //     console.log (error);
+    //     let message = error.statusText || "Ocurrio un error";
+    //     $template.innerHTML = `
+    //     <div class="card">
+    //     <p> Error ${err.status}: ${message}</p>
+    //     <img src="${rickMorty.image}" alt="${rickMorty.name}">
+    //     <h3>${rickMorty.name}</h3>
+    // //     <p>${rickMorty.species}</p>
+    // </div>
+
+    }
+    $gridContainer.innerHTML = $template;
+  } catch (error) {
+    console.log(error);
+    let message = error.statusText || "Ocurrio error";
+    $gridContainer.innerHTML = `<p> Error ${error.status}: ${message}</p>`
+  } 
+}
