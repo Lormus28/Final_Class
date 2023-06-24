58270582fc209b8f6fb80c5da8e1e3843c3b4886
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-06-24 09:40:39 -0500

    consumo api

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector(".title");
+const rickAndMorty = "https://rickandmortyapi.com/api/character";
 
 btn.addEventListener('click', () => {
     console.log("btn");
+    callApirickAndMorty();
 });
 
+const callApirickAndMorty= async ()=>{
+    const res = await fetch(rickAndMorty);
+    console.log(res)
+    const data = await res.json();
+    console.log (data);
+} 
 
